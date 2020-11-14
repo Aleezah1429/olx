c4862f6849584e67731fa31b42e73e4fcbb55c1c
Aleezah1429 <64039135+Aleezah1429@users.noreply.github.com>
2020-11-14 23:41:06 +0500

    Add files via upload

---
+++ build/precache-manifest.7d0956ec61f0b0448c9f53af7c9917bf.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64fa9f30542ce07aded75f080148e014",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d707f9be26864ab7c97",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "61e929f6c98b5f6d8c6f",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "3d707f9be26864ab7c97",
+    "url": "/static/js/2.cd270f3c.chunk.js"
+  },
+  {
+    "revision": "bca29e7cb811c1469fecd515de5f0d93",
+    "url": "/static/js/2.cd270f3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61e929f6c98b5f6d8c6f",
+    "url": "/static/js/main.4c3b87c1.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  },
+  {
+    "revision": "4b657dad66070b01e0b897dceb17796b",
+    "url": "/static/media/Add.4b657dad.png"
+  },
+  {
+    "revision": "1062d562a559964979b14fc73094e34a",
+    "url": "/static/media/AppStore.1062d562.PNG"
+  },
+  {
+    "revision": "ff75f41f115d30224ce3af4a4aebd871",
+    "url": "/static/media/FB.ff75f41f.PNG"
+  },
+  {
+    "revision": "c8863168770e0aa7092f561d92438a56",
+    "url": "/static/media/GooglePlay.c8863168.PNG"
+  },
+  {
+    "revision": "7096c6bc939d07c53d0dab7571673f4c",
+    "url": "/static/media/Instagram.7096c6bc.PNG"
+  },
+  {
+    "revision": "a0cb23780ad23591870c2c738f15f1ec",
+    "url": "/static/media/Logo.a0cb2378.PNG"
+  },
+  {
+    "revision": "fd9d54c029c28d0db2bf213a13a72c35",
+    "url": "/static/media/Twitter.fd9d54c0.PNG"
+  },
+  {
+    "revision": "8337c6d0df086f3aa2a74d2cf000d682",
+    "url": "/static/media/img.8337c6d0.png"
+  },
+  {
+    "revision": "0044653cc4717ddf8135bce7106f1f07",
+    "url": "/static/media/playIcon.0044653c.PNG"
+  },
+  {
+    "revision": "c6040c2a240d898d6c92ccef1e09b600",
+    "url": "/static/media/sellCard.c6040c2a.PNG"
+  }
+]);
